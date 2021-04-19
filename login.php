27a4675972d52d9e7a082c8e102a69b4c8cd0d38
SilverBlack7 <82745126+SilverBlack7@users.noreply.github.com>
2021-04-19 11:15:54 +0400

    Update login.php

--- login.php
+++ login.php
@@ -46,7 +46,7 @@ if(isset($_POST['submit_btn']))
 	}
 	else
 	{
-		echo '<h4 style= "color:red"> Username doesn't exist</h4>';
+		echo '<h4 style= "color:red"> Username doesn\'t exist</h4>';
 	}
 }
 else{
